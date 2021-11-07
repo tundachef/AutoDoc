591d3eb668da53a3e039e4de275136ddd08632cc
MuhammedKasujja <40717406+MuhammedKasujja@users.noreply.github.com>
2021-11-07 14:02:06 +0300

    Removed all cached files

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Str;
 | model instances for testing / seeding your application's database.
 |
 */
-
+  
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
